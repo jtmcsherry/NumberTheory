c0c8aca36b38ec8c464f5e7975c4f6d5f67479de
Jack Mcsherry <146376839+jtmcsherry@users.noreply.github.com>
2025-04-15 15:41:37 -0700

    Initial commit

---
+++ src/PrimeFactor.java
@@ -0,0 +1,31 @@
+public class PrimeFactor {
+    private final int value;
+    private int exponent;
+
+    public PrimeFactor(int value, int exponent) {
+        this.value = value;
+        this.exponent = exponent;
+    }
+
+    public void increaseExponent() {
+        exponent++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getExponent() {
+        return exponent;
+    }
+
+    /*
+    public int getTotalValue(){
+        return (int)Math.pow(value, exponent);
+    }
+     */
+
+    public String toString(PrimeFactor p) {
+        return p.value + "^" + p.exponent;
+    }
+}
