a7d8e57fe50d857e03daa0a72f238b7cbc067adc
jtmcsherry <jtmcsherry@dons.usfca.edu>
2026-05-16 16:48:21 -0700

    missed some things

--- src/main/java/mathcircles/Main.java
+++ src/main/java/mathcircles/Main.java
@@ -3,9 +3,9 @@ package mathcircles;
 public class Main {
     public static void main(String[] args) {
         CollatzConjecture cc = new CollatzConjecture(1000000);
-        CC.findMaxChain();
+        cc.findMaxChain();
 
         LychrelNumbers ln = new LychrelNumbers(10000);
-        System.out.println("The number of Lychrel Numbers below 10,000 is " + LN.numLychrel());
+        System.out.println("The number of Lychrel Numbers below 10,000 is " + ln.numLychrel());
     }
 }
